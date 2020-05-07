8d4b9c707b8a38b04ddb6fcce6fde4f0db951e45
nerrakik <nahlaerrakik@gmail.com>
2020-05-07 19:13:26 +0000

    update tic tac toe

--- Tic Tac Toe/src/gui/BoardPanel.java
+++ Tic Tac Toe/src/gui/BoardPanel.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
@@ -8,21 +10,19 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
+
 import logic.Board;
 import logic.Grid;
 import logic.Player;
 
 public class BoardPanel extends JPanel implements MouseListener{
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private Board board = new Board();
-	private Player player = new Player("test", 'X');
+	private Player player = new Player("Zouhair", 'X');
 	private Player computer = new Player("computer", 'O');
 	private boolean gameOver, gameWon, gameTie;
 	private int mousePressX, mousePressY;
-	private int remaningGrids = Board.SIZE*Board.SIZE;
+	private int remainingGrids = Board.SIZE*Board.SIZE;
 	
 	private ArrayList<Point> coordinates = new ArrayList<Point>(Board.SIZE);
 	
@@ -31,18 +31,14 @@ public class BoardPanel extends JPanel implements MouseListener{
 		this.addMouseListener(this);		
 	}
 	
+	@Override
 	public void paint(Graphics graphic){
 		super.paint(graphic);
+		
 		drawBoard(graphic);
 		
-		if(this.gameWon)
-			setBackground(Color.green);
-		
-		if(this.gameOver)
-			setBackground(Color.red);
-		
-		if(this.gameTie)
-			setBackground(Color.orange);
+		if(this.gameWon || this.gameOver || this.gameTie)
+			drawGameResult(graphic);
 	}
 	
 	private void drawBoard(Graphics graphic) {
@@ -55,8 +51,6 @@ public class BoardPanel extends JPanel implements MouseListener{
 				graphic.drawRect(xsquare, ysquare, squareWidth, squareHeight);
 				
 				if(this.board.get(i,j).isClicked()) {
-					graphic.setColor(Color.black);
-					
 					int x = this.board.get(i,j).getX() * (squareWidth) + (squareWidth / 2);
 					int y = (this.board.get(i,j).getY() + 1) * (squareHeight) - (squareHeight / 2);
 					
@@ -70,6 +64,37 @@ public class BoardPanel extends JPanel implements MouseListener{
 	}
 	
 
+	private void drawGameResult(Graphics graphic) {
+		Font myFont = new Font ("Courier New", 1, 17);
+		graphic.setFont (myFont);
+		
+		String str;
+		Color backgroundColor;
+		if(this.gameWon) {
+			str = "Congratulations, You Won!";
+			backgroundColor = Color.green;
+		}
+			
+		else if(this.gameOver) {
+			str = "Game Over, You Lost!";
+			backgroundColor = Color.red;
+		}
+		else {
+			str = "It's a tie!";
+			backgroundColor = Color.orange;
+		}
+						
+		FontMetrics myFontMetrics = graphic.getFontMetrics();
+		int stringWidth = myFontMetrics.stringWidth(str);
+        int stringAccent = myFontMetrics.getAscent();
+        
+        int x = getWidth() / 2 - stringWidth / 2;
+        int y = getHeight() / 2 + stringAccent / 2;
+        
+		graphic.drawString (str, x, y);
+		setBackground(backgroundColor);
+	}
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if(this.gameOver || this.gameWon || this.gameTie)
@@ -88,8 +113,8 @@ public class BoardPanel extends JPanel implements MouseListener{
 			int maxY = (int)this.coordinates.get(i).getY() + squareHeight;
 
 			if ((this.mousePressX >= minX && this.mousePressX <= maxX) && (this.mousePressY >= minY && this.mousePressY <= maxY)) {
-				int x = minX/squareWidth;
-				int y = minY/squareHeight;
+				int x = minX / squareWidth;
+				int y = minY / squareHeight;
 				
 				//Ignore mouse click if Grid is already clicked
 				if(this.board.get(x,y).isClicked())
@@ -97,14 +122,14 @@ public class BoardPanel extends JPanel implements MouseListener{
 				
 				this.board.get(x,y).setClicked(true);
 				this.board.get(x,y).setValue(this.player.getSign());
-				this.remaningGrids--;
+				this.remainingGrids--;
 				
 				boolean playerWon = this.board.checkBoard(this.player.getSign());
 				if (playerWon) {
 					this.gameWon = true;
 					break;
 				}				
-				else if (this.remaningGrids>0){
+				else if (this.remainingGrids>0){
 					//Setup computer move
 					Grid randomGrid = Grid.GetRandomGrid(Board.SIZE);
 					while(this.board.get(randomGrid.getX(), randomGrid.getY()).isClicked()) {
@@ -113,7 +138,7 @@ public class BoardPanel extends JPanel implements MouseListener{
 					
 					this.board.get(randomGrid.getX(),randomGrid.getY()).setClicked(true);
 					this.board.get(randomGrid.getX(),randomGrid.getY()).setValue(this.computer.getSign());
-					this.remaningGrids--;
+					this.remainingGrids--;
 					
 					boolean computerWon = this.board.checkBoard(this.computer.getSign());
 					if (computerWon) {
--- Tic Tac Toe/src/gui/Game.java
+++ Tic Tac Toe/src/gui/Game.java
@@ -1,25 +1,43 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
-
-public class Game extends JFrame{
+public class Game extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
 	BoardPanel boardPanel = new BoardPanel();
+	JButton startNewGame = new JButton("Start a new game");
 	
 	public Game() {		
 		this.setLayout(new BorderLayout());
 		this.getContentPane().add(this.boardPanel, BorderLayout.CENTER);
+		this.getContentPane().add(this.startNewGame, BorderLayout.SOUTH);
 		this.setSize(500, 500);
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		this.startNewGame.addActionListener(this);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.setVisible(false);
+		@SuppressWarnings("unused")
+		Game newGame = new Game();
+		
 	}
 	
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
 		Game myGame = new Game();
 	}
+
+	
 
 }
